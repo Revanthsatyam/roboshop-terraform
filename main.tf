a69e639da1d7e91adff5ef02e1ce96afc882baf1
Revanthsatyam <satyamrevanth@gmail.com>
2023-10-28 18:01:14 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -11,6 +11,17 @@ module "vpc" {
   env                        = var.env
 }
 
-output "vpc" {
-  value = module.vpc
+module "alb" {
+  source          = "git::https://github.com/Revanthsatyam/tf-module-alb.git"
+
+  for_each        = var.alb
+  internal        = each.value["internal"]
+  lb_type         = each.value["lb_type"]
+  sg_ingress_cidr = each.value["sg_ingress_cidr"]
+  vpc_id          = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+  subnets         = each.value["internal"] ? data.aws_subnets.subnets : local.app_subnets
+  tags            = var.tags
+  env             = var.env
+  sg_port         = each.value["sg_port"]
 }
+
