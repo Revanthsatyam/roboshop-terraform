d5bf3bf69dc183d45dcfe55cb2afd6d9607a6248
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-19 11:37:00 +0530

    Terraform S3

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {}
-}
+module "components" {
+  source = "git::https://github.com/Revanthsatyam/tf-module-basic-test.git"
 
-variable "test" {}
-
-output "test" {
-  value = var.test
+  for_each = var.components
+  vpc_security_group_ids = var.vpc_security_group_ids
+  zone_id = var.zone_id
+  instance_type = each.value["instance_type"]
+  name = each.value["name"]
 }
