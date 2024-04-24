c579b2c77c1aa2f519473f0244c563c8b6330309
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-24 13:35:20 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "components" {
-  source = "git::https://github.com/Revanthsatyam/tf-module-vpc-test.git"
+  source = "git::https://github.com/Revanthsatyam/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr     = each.value["cidr"]
