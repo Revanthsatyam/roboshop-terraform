b4fbe03e9277e0f16d045a4785163265f6cc3ffe
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-29 10:23:41 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -39,6 +39,8 @@ module "docdb" {
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot     = each.value["skip_final_snapshot"]
   engine_version          = each.value["engine_version"]
+  instance_count          = each.value["instance_count"]
+  instance_class          = each.value["instance_class"]
 }
 
 
