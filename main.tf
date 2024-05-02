5be5f50e76f77554ef6f10730f56d17b8b878d2a
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-02 16:13:04 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ module "app" {
   subnet_ids      = local.app_subnets
 
   alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
-  alb_listener = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+  #alb_listener = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
 }
 
 
