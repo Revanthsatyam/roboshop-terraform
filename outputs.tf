defe686a09dfa9899e51a880cf132295819dd48c
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-03 12:33:32 +0530

    Terraform

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "alb" {
-  value = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
-}
+#output "alb" {
+#  value = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+#}
