86dc9aec078eb21e62c9d9f75fb927649415f12b
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-18 20:12:24 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -127,9 +127,3 @@ module "app" {
   public_alb_name  = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
   public_listener  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
 }
-
-
-
-
-
-
