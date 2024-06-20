fab8532df0065b5b82d0a7d7a96827838b57a6ee
Revanthsatyam <satyamrevanth@gmail.com>
2024-06-20 19:46:00 +0530

    Changes

--- main.tf
+++ main.tf
@@ -143,12 +143,52 @@ module "rabbitmq" {
 #   }
 # }
 
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 19.0"
+#
+#   cluster_name    = "prod-roboshop"
+#   cluster_version = "1.28"
+#
+#   cluster_endpoint_public_access  = false
+#
+#   cluster_addons = {
+#     coredns = {
+#       most_recent = true
+#     }
+#     kube-proxy = {
+#       most_recent = true
+#     }
+#     vpc-cni = {
+#       most_recent = true
+#     }
+#   }
+#
+#   vpc_id                   = local.vpc_id
+#   subnet_ids               = local.app_subnets
+#   control_plane_subnet_ids = local.app_subnets
+#
+#
+#   eks_managed_node_groups = {
+#     green = {
+#       min_size     = 1
+#       max_size     = 10
+#       desired_size = 1
+#
+#       instance_types = ["t3.large"]
+#       capacity_type  = "SPOT"
+#     }
+#   }
+#
+#   tags = var.tags
+# }
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+  version = "~> 20.0"
 
   cluster_name    = "prod-roboshop"
-  cluster_version = "1.28"
+  cluster_version = "1.30"
 
   cluster_endpoint_public_access  = false
 
@@ -168,9 +208,8 @@ module "eks" {
   subnet_ids               = local.app_subnets
   control_plane_subnet_ids = local.app_subnets
 
-
   eks_managed_node_groups = {
-    green = {
+    Blue = {
       min_size     = 1
       max_size     = 10
       desired_size = 1
@@ -179,6 +218,11 @@ module "eks" {
       capacity_type  = "SPOT"
     }
   }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  # enable_cluster_creator_admin_permissions = true
+
 
   tags = var.tags
 }
