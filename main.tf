4cd13a2fe1b22bda7d38e8c2f374f2c2bea23776
Revanthsatyam <satyamrevanth@gmail.com>
2024-06-27 17:02:30 +0530

    Changes

--- main.tf
+++ main.tf
@@ -133,15 +133,15 @@ module "rabbitmq" {
 #   public_listener  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
 # }
 #
-# resource "aws_instance" "web" {
-#   ami                    = data.aws_ami.ami.id
-#   vpc_security_group_ids = ["sg-0f15c7e71393537f6"]
-#   instance_type          = "t3.medium"
-#
-#   tags = {
-#     Name = "load-runner"
-#   }
-# }
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ami.id
+  vpc_security_group_ids = ["sg-0f15c7e71393537f6"]
+  instance_type          = "t3.medium"
+
+  tags = {
+    Name = "load-runner"
+  }
+}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -174,7 +174,7 @@ module "eks" {
       max_size     = 10
       desired_size = 3
 
-      instance_types = ["t3.large"]
+      instance_types = ["t3.2xlarge"]
       capacity_type  = "SPOT"
     }
   }
